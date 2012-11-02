c4c3c8e6a4bd8f5485851e49feef02e97c3a39c9
Jules Olleon <jolleon@gmail.com>
2012-11-02 11:33:43 -0700

    changed desk add/remove logic

--- app/desk.py
+++ app/desk.py
@@ -45,56 +45,67 @@ class DeskPage(webapp2.RequestHandler):
     @authenticate
     def post(self):
         map_id = self.request.get('map_id') or "default"
-        desk_id = self.request.get('desk_id')
-        method = self.request.get('method')
-
+        action = self.request.get('action')
+        
         self.msg = Message()
         try:
-            if method and method == "delete":
-                self.do_delete(map_id, desk_id)
+            if action and action == "remove":
+                self.do_delete(map_id)
             else:
-                self.do_post(map_id, desk_id)
+                self.do_post(map_id)
         except Exception as e:
             self.msg.error(e)
         self.response.out.write(self.msg.format())
 
 
-    def do_post(self, map_id, desk_id):
-        if desk_id:
-            desk = self.lookup_desk(map_id, desk_id)
-            if not desk:
-                self.msg.error("No desk found for id %s in map %s" % (desk_id, map_id))
-                return
+    def do_post(self, map_id):
+        # check if this desk is already here
+        desk = self.lookup_desk(map_id)
+        if desk:
+            # desk is here: do nothing
+            pass
         else:
             desk = Desk(parent=desk_key(map_id))
 
-        try:
-            desk.x = int(self.request.get('x'))
-            desk.y = int(self.request.get('y'))
-            desk.put()
+            try:
+                desk.x = int(self.request.get('x'))
+                desk.y = int(self.request.get('y'))
+                desk.put()
 
-            self.msg.ok(desk.to_dic())
-        except ValueError:
-            self.msg.error("Invalid coordinates")
+            except ValueError:
+                self.msg.error("Invalid coordinates")
+                return
 
-            
-    def do_delete(self, map_id, desk_id):
-        if not desk_id:
-            self.msg.error("Please specify desk_id") 
-            return
-        
-        desk = self.lookup_desk(map_id, desk_id)
-        if not desk:
-            self.msg.error("No desk found for id %s in map %s" % (desk_id, map_id))
-            return
-         
-        desk.delete()
         self.msg.ok(desk.to_dic())
+            
+    def do_delete(self, map_id):
+        # check if this desk is already here
+        desk = self.lookup_desk(map_id)
+        if not desk:
+            # no desk here already, do nothing
+            self.msg.ok("No desk here, doing nothing")
+        
+        else:
+            desk.delete()
+            self.msg.ok(desk.to_dic())
 
 
-    def lookup_desk(self, map_id, desk_id): 
-        key = db.Key.from_path('Desk', map_id, 'Desk', int(desk_id))
-        desk = db.get(key)
+    def lookup_desk(self, map_id): 
+        desk_id = self.request.get('desk_id')
+        if not desk_id:
+            x = int(self.request.get('x'))
+            y = int(self.request.get('y'))
+            if not x or not y:
+                self.msg.error("Please specify desk_id or x and y")
+                return None
+
+        if desk_id:
+            key = db.Key.from_path('Desk', map_id, 'Desk', int(desk_id))
+            desk = db.get(key)
+        else:
+            q = db.GqlQuery("SELECT * FROM Desk WHERE ANCESTOR IS :1 " +
+                        "AND x = :2 AND y = :3", desk_key(map_id), x, y)
+            desk = q.get()
         return desk
 
 
