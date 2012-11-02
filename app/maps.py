bd54fd624851f4dd3b5a34b546de6d6b681e73cb
Jules Olleon <jolleon@gmail.com>
2012-11-02 12:52:38 -0700

    added maps

---
+++ app/maps.py
@@ -0,0 +1,84 @@
+import webapp2
+import json
+from util import *
+
+from google.appengine.ext import db
+from google.appengine.api import users
+
+
+class Map(db.Model):
+    tl_x = db.IntegerProperty()
+    tl_y = db.IntegerProperty()
+    br_x = db.IntegerProperty()
+    br_y = db.IntegerProperty()
+    map_id = db.StringProperty()
+
+    def to_dic(self):
+        return {'tl_x': self.tl_x,
+                'tl_y': self.tl_y,
+                'tl_x': self.tl_x,
+                'tl_y': self.tl_y,
+                'map_id': self.map_id}
+
+    def to_json(self):
+        return json.dumps(self.to_dic())
+
+
+class MapsPage(webapp2.RequestHandler):
+
+    @authenticate
+    def get(self):
+        self.msg = Message()
+        map_id = self.request.get('map_id')
+
+        try: 
+            if map_id:       
+                maps = db.GqlQuery("SELECT * FROM Map WHERE map_id = :1", map_id)
+            else:
+                maps = db.GqlQuery("SELECT * FROM Map")
+            maps_l = [m.to_dic() for m in maps]
+            self.msg.ok(maps_l)
+        except Exception as e:
+            self.msg.error(e)        
+        self.response.out.write(self.msg.format())
+
+
+    @authenticate
+    def post(self):
+        self.msg = Message()
+        try:
+            self.do_post()
+        except Exception as e:
+            self.msg.error(e)
+        self.response.out.write(self.msg.format())
+
+
+    def do_post(self):
+        map_id = self.request.get('map_id')
+        if not map_id:
+            self.msg.error("Please specify map_id")
+            return
+
+        m = lookup_map(map_id) 
+
+        if not m:
+            m = Map(key_name=map_id)
+
+        try:
+            m.tl_x = int(self.request.get('tl_x'))
+            m.tl_y = int(self.request.get('tl_y'))
+            m.br_x = int(self.request.get('br_x'))
+            m.br_y = int(self.request.get('br_y'))
+            m.map_id = self.request.get('map_id')
+            m.put()
+
+            self.msg.ok(m.to_dic()) 
+        except ValueError:
+            self.msg.error("Invalid parameters")
+
+            
+def lookup_map(id): 
+    key = db.Key.from_path('Map', id)
+    return db.get(key)
+
+
--- app/visualoffice.py
+++ app/visualoffice.py
@@ -1,6 +1,8 @@
 import webapp2
 import desk
 import user
+import maps
+
 from util import authenticate
 
 
@@ -39,11 +41,37 @@ xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xmlhttp.send("x=5&y=6&map_id=8thfloor");
 }
 </script>
-</head>
-<body>
+<script>
+function loadXMLDoc2()
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("POST","maps",true);
+xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xmlhttp.send("tl_x=-5&tl_y=-8&br_x=100&br_y=200&map_id=9thfloor");
+}
+</script>
 <br>
 <button type="button" onclick="loadXMLDoc()">Post data</button>
+<br>
+<button type="button" onclick="loadXMLDoc2()">Post data</button>
 <div id="myDiv"></div>
+
+
 """)
 
 
@@ -52,5 +80,6 @@ app = webapp2.WSGIApplication([('/desks', desk.ListDesksPage),
                                ('/users', user.ListUsersPage),
                                ('/user', user.UserPage),
                                ('/me', user.MePage),
+                               ('/maps', maps.MapsPage),
                                ('/', MainPage)],
                               debug=True)
